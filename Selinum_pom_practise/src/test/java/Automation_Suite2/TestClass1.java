0f6648865b3e788884b8d4d8abe5c9fd0ed7b1a3
alekhya anumalasetty <alekhya anumalasetty@192.168.0.24>
2018-09-19 21:50:58 -0400

    just commiting

---
+++ Selinum_pom_practise/src/test/java/Automation_Suite2/TestClass1.java
@@ -0,0 +1,21 @@
+package Automation_Suite2;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+  
+	
+	@Test
+  public void C1M1(){
+		System.out.println("C1M1");
+}
+	@Test
+	  public void C1M2(){
+			System.out.println("C1M2");
+
+
+
+}
+	
+	
+}
---
+++ Selinum_pom_practise/src/test/java/Automation_Suite2/TestClass2.java
@@ -0,0 +1,19 @@
+package Automation_Suite2;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+  @Test
+  public void C21M1(){
+			System.out.println("C2M1");
+	}
+		@Test
+		  public void C2M2(){
+				System.out.println("C2M2");
+
+
+
+	}
+		
+  }
+
