220128b22d1ff5aa24a8ab83c6942a73f3958848
alekhya anumalasetty <alekhya anumalasetty@192.168.0.24>
2018-09-19 22:54:54 -0400

    with readwrite config

---
+++ Selinum_pom_practise/src/main/java/utils/Reading_configproperties_file.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Reading_configproperties_file {
+
+	
+	public String readWriteConfig(String key)
+	
+{
+		String value = "";
+		try
+		{
+			Properties prop = new Properties();
+			FileInputStream Filenew = new FileInputStream("src//test//resources//properties_file//config.properties"); 
+			prop.load(Filenew);
+		value=	prop.getProperty(key);
+			
+		}
+		catch(Exception e)		
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		return value;
+		
+
+}
+	
+	
+}
--- Selinum_pom_practise/src/test/java/Automation_Suite2/TestClass1.java
+++ Selinum_pom_practise/src/test/java/Automation_Suite2/TestClass1.java
@@ -2,17 +2,23 @@ package Automation_Suite2;
 
 import org.testng.annotations.Test;
 
+import utils.Reading_configproperties_file;
+
 public class TestClass1 {
   
 	
 	@Test
   public void C1M1(){
-		System.out.println("C1M1");
+		
+		Reading_configproperties_file Reading_configproperties_file_obj = new Reading_configproperties_file();
+				
+		String url = Reading_configproperties_file_obj.readWriteConfig("appurl");
+		System.out.println(url);
+		
+		
 }
-	@Test
-	  public void C1M2(){
-			System.out.println("C1M2");
-}
+	
+
 	
 	
 }
